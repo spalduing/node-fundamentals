e606bda838d87cdcd687098e9e565f6716615e18
mdiazc <spalduing12@gmail.com>
2022-05-10 12:11:35 -0500

    Added: streams in nodejs

---
+++ memory/stream.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const stream = require('stream');
+const util = require('util');
+let data = '';
+
+let readableStream = fs.createReadStream(__dirname + '/input.txt');
+readableStream.setEncoding('UTF8');
+
+readableStream.on('data', (chunk) => {
+  data += chunk;
+});
+
+readableStream.on('end', () => {
+  console.log(data.toString());
+});
+
+process.stdout.write('hola');
+process.stdout.write('que');
+process.stdout.write('tal');
+
+// const Transform = stream.Transform;
+
+// function Mayus() {
+//   Transform.call(this);
+// }
+
+// util.inherits(Mayus, Transform);
+
+// Mayus.prototype._transform = function (chunck, codif, cb) {
+//   chunckMayus = chunck.toString().toUpperCase();
+//   this.push(chunckMayus);
+//   cb();
+// };
+
+// let mayus = new Mayus();
+
+// readableStream.pipe(mayus).pipe(process.stdout);
